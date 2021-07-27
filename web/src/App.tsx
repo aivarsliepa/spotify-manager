209975c6bb7750904b69bc94b725b70e36991b66
Aivars Liepa <aivarsliepa1992@gmail.com>
2021-07-27 20:26:29 +0300

    add material UI

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Provider } from "react-redux";
+import CssBaseline from "@material-ui/core/CssBaseline";
 
 import AppRouter from "./router/AppRouter";
 import { store } from "./store";
@@ -7,6 +8,7 @@ import { store } from "./store";
 const App: React.FC = () => {
   return (
     <React.StrictMode>
+      <CssBaseline />
       <Provider store={store}>
         <AppRouter />
       </Provider>
