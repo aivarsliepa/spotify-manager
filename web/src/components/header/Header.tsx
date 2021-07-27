8544db4747cdf16910e855833ceba14975e9a4fa
Aivars Liepa <aivarsliepa1992@gmail.com>
2021-07-27 20:28:03 +0300

    add header

--- web/src/components/header/Header.tsx
+++ web/src/components/header/Header.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useCallback } from "react";
+import { useHistory } from "react-router-dom";
+import AppBar from "@material-ui/core/AppBar";
+import Box from "@material-ui/core/Box";
+import Toolbar from "@material-ui/core/Toolbar";
+
+import { IconButton } from "@material-ui/core";
+import HomeIcon from "../icons/HomeIcon";
 
 const Header: React.FC = () => {
-  return <div>Header works!</div>;
+  const history = useHistory();
+  const onHomeIconClick = useCallback(() => history.push("/"), [history]);
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" color="inherit" aria-label="home" onClick={onHomeIconClick}>
+            <HomeIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 };
 
 export default Header;
--- web/src/router/AppRouter.tsx
+++ web/src/router/AppRouter.tsx
@@ -1,20 +1,51 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import { Route, Switch, Redirect, BrowserRouter } from "react-router-dom";
+import Box from "@material-ui/core/Box";
+import Container from "@material-ui/core/Container";
 
 import Shell from "../components/shell/Shell";
 import Header from "../components/header/Header";
-import LoggedInRoutes from "./LoggedInRoutes";
-import LoggedOutRoutes from "./LoggedOutRoutes";
 import { useAppSelector } from "../store/hooks";
 import { selectLoggedIn } from "../store/authSlice";
+import Main from "../pages/Main";
+import Songs from "../pages/Songs";
+import Playlists from "../pages/Playlists";
+import SongDetailPage from "../pages/SongDetailPage";
+import PlaylistDetails from "../pages/PlaylistDetails";
+import Index from "../pages/Index";
+
+const LoggedInRoutes: React.FC = () => (
+  <>
+    <Route exact path="/" component={Main} />
+    <Route exact path="/songs" component={Songs} />
+    <Route exact path="/songs/:songId" component={SongDetailPage} />
+    <Route exact path="/playlists" component={Playlists} />
+    <Route exact path="/playlists/:playlistId" component={PlaylistDetails} />
+    <Redirect to="/" />
+  </>
+);
+
+const LoggedOutRoutes: React.FC = () => (
+  <>
+    <Route exact path="/" component={Index} />
+    <Redirect to="/" />
+  </>
+);
 
 const AppRouter: React.FC = () => {
   const loggedIn = useAppSelector(selectLoggedIn);
 
   return (
     <Shell>
-      <Header />
-      <BrowserRouter>{loggedIn ? <LoggedInRoutes /> : <LoggedOutRoutes />}</BrowserRouter>
+      <BrowserRouter>
+        <Header />
+        <Container>
+          <Box paddingY={3}>
+            <Switch>{loggedIn ? <LoggedInRoutes /> : <LoggedOutRoutes />}</Switch>
+          </Box>
+        </Container>
+      </BrowserRouter>
+      {/* Footer ? */}
     </Shell>
   );
 };
